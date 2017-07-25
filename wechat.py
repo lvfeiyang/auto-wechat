b8441ea70afe56a92bdc6a1ba09cb6f26d24b0c3
lvfeiyang <lxm19900207@163.com>
2017-07-25 11:16:48 +0800

    demo finish

--- wechat.py
+++ wechat.py
@@ -35,5 +35,72 @@ def test():
 
     driver.quit()
 
+def view():
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = 'Lenovo K50-t5'
+    # desired_caps['app'] = PATH('weixin6510android1080.apk')
+    desired_caps['appPackage'] = 'com.tencent.mm'
+    desired_caps['appActivity'] = '.ui.LauncherUI'
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    sleep(5)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("腾讯新闻")').click()
+    sleep(2)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("男子越狱潜逃17年 被抓前资产过亿公司将上市(组图)")').click()
+    sleep(2)
+
+    driver.quit()
+
+def changeUser():
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = 'Lenovo K50-t5'
+    # desired_caps['app'] = PATH('weixin6510android1080.apk')
+    desired_caps['appPackage'] = 'com.tencent.mm'
+    desired_caps['appActivity'] = '.ui.LauncherUI'
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    sleep(5)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("我")').click()
+    sleep(1)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("设置")').click()
+    sleep(1)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("退出")').click()
+    sleep(1)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("退出当前帐号")').click()
+    sleep(1)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("退出")').click()
+    sleep(9)
+
+    driver.find_element_by_accessibility_id('更多').click()
+    sleep(1)
+
+    driver.find_element_by_android_uiautomator('new UiSelector().text("切换帐号")').click()
+    sleep(1)
+
+    textfield = driver.find_element_by_class_name("android.widget.EditText")
+    textfield.send_keys("17317630207")
+    driver.find_element_by_android_uiautomator('new UiSelector().text("下一步")').click()
+    sleep(1)
+
+    textfields = driver.find_elements_by_class_name("android.widget.EditText")
+    textfields[1].send_keys("Lxm784826")
+    driver.find_element_by_android_uiautomator('new UiSelector().text("登录")').click()
+    sleep(5)
+
+    driver.quit()
+
 if __name__=="__main__":
-    test()
+    # test()
+    # view()
+    changeUser()
